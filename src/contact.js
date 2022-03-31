5033539946c5064418cf4d74d850834067636a01
Abhinandvk <abhinandazokdio@gmail.com>
2022-03-31 21:43:42 +0530

    updated

---
+++ src/contact.js
@@ -0,0 +1,17 @@
+/** @format */
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <label>Email ID</label>
+            <input type='email' />
+            <br />
+            <label>Message</label>
+            <textarea></textarea>
+            <button>Send</button>
+        </div>
+    );
+}
+
+export default Contact;
